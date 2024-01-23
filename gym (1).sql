d59c22068c9d625642109bf350161c77c137bf43
Carnacky79 <f.lombardi79@gmail.com>
2024-01-23 16:48:09 +0100

    New SQL dump

--- gym (1).sql
+++ gym (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Gen 04, 2024 alle 07:11
+-- Creato il: Gen 23, 2024 alle 15:47
 -- Versione del server: 5.7.40-log
 -- Versione PHP: 8.1.3
 
@@ -61,14 +61,6 @@ CREATE TABLE `esercizi` (
 --
 
 INSERT INTO `esercizi` (`id`, `nome`, `id_categoria`) VALUES
-(9, 'affondi_supini', 1),
-(10, 'curl_con_bilanciere', 1),
-(11, 'curl_hammer', 1),
-(12, 'curl_su_panca_inclinata', 1),
-(13, 'dip_tricipiti', 1),
-(14, 'flessione_dei_tricipiti', 1),
-(15, 'french_press_unilaterale', 1),
-(16, 'pressa_per_tricipiti', 1),
 (17, 'affondi_supini', 1),
 (18, 'curl_con_bilanciere', 1),
 (19, 'curl_hammer', 1),
@@ -129,7 +121,17 @@ CREATE TABLE `esercizi_scheda` (
 
 INSERT INTO `esercizi_scheda` (`id_scheda`, `id_esercizio`, `rep`, `serie`, `recupero`, `durata`, `carico`) VALUES
 (1, 27, 8, 3, 45, NULL, 30),
-(1, 21, 10, 3, 45, NULL, 30);
+(1, 21, 10, 3, 45, NULL, 30),
+(26, 9, 34, 2, NULL, NULL, 35),
+(26, 39, 44, 43, NULL, NULL, 90),
+(26, 41, 44, 22, NULL, NULL, 90),
+(36, 22, 45, 3, NULL, NULL, 90),
+(36, 22, 45, 3, NULL, NULL, 90),
+(36, 22, 45, 3, NULL, NULL, 90),
+(37, 47, NULL, NULL, NULL, NULL, NULL),
+(37, 48, NULL, NULL, NULL, NULL, NULL),
+(38, 17, 12, 4, 60, NULL, 45),
+(38, 18, 12, 4, 50, NULL, 44);
 
 -- --------------------------------------------------------
 
@@ -152,8 +154,8 @@ CREATE TABLE `palestre` (
 --
 
 INSERT INTO `palestre` (`id`, `nome`, `indirizzo`, `citta`, `email`, `password`, `telefono`) VALUES
-(1, 'Palestra di prova', 'via qualcosa', 'Napoli', 'ciao@ciao.it', 'password', '43353'),
-(2, 'seconda palestra', 'via di prova', 'Napoli', 'fra@fra.com', 'password', '8758765');
+(1, 'Ciro', 'via qualcosa', 'Napoli', 'ciao@ciao.it', 'password', '43353'),
+(2, 'Ciro', 'via di prova', 'Napoli', 'fra@fra.com', 'password', '8758765');
 
 -- --------------------------------------------------------
 
@@ -163,7 +165,7 @@ INSERT INTO `palestre` (`id`, `nome`, `indirizzo`, `citta`, `email`, `password`,
 
 CREATE TABLE `schede` (
   `id` int(11) NOT NULL,
-  `data` date DEFAULT NULL,
+  `data` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `id_utente` int(11) DEFAULT NULL,
   `attiva` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -173,7 +175,43 @@ CREATE TABLE `schede` (
 --
 
 INSERT INTO `schede` (`id`, `data`, `id_utente`, `attiva`) VALUES
-(1, '2023-12-29', 1, 1);
+(1, '2023-12-28 23:00:00', 1, 1),
+(2, '2023-12-06 23:00:00', 1, 0),
+(4, '2024-01-14 13:13:04', 1, 0),
+(5, '2024-01-18 23:14:02', 1, 0),
+(6, '2024-01-18 23:14:52', 1, 0),
+(7, '2024-01-18 23:23:43', 1, 0),
+(8, '2024-01-18 23:24:12', 1, 0),
+(9, '2024-01-18 23:28:57', 1, 0),
+(10, '2024-01-18 23:30:07', 1, 0),
+(11, '2024-01-18 23:30:40', 1, 0),
+(12, '2024-01-18 23:41:55', 1, 0),
+(13, '2024-01-18 23:43:44', 1, 0),
+(14, '2024-01-18 23:44:21', 1, 0),
+(15, '2024-01-18 23:47:51', 1, 0),
+(16, '2024-01-18 23:48:44', 1, 0),
+(17, '2024-01-18 23:49:29', 1, 0),
+(18, '2024-01-18 23:49:50', 1, 0),
+(19, '2024-01-18 23:52:38', 1, 0),
+(20, '2024-01-18 23:53:04', 1, 0),
+(21, '2024-01-18 23:56:55', 1, 0),
+(22, '2024-01-18 23:57:14', 1, 0),
+(23, '2024-01-18 23:57:56', 1, 0),
+(24, '2024-01-18 23:58:35', 1, 0),
+(25, '2024-01-19 00:00:56', 1, 0),
+(26, '2024-01-19 00:17:49', 1, 0),
+(27, '2024-01-19 09:38:37', 1, 0),
+(28, '2024-01-19 09:39:59', 1, 0),
+(29, '2024-01-19 09:41:08', 1, 0),
+(30, '2024-01-19 09:43:57', 1, 0),
+(31, '2024-01-19 09:45:46', 1, 0),
+(32, '2024-01-19 09:47:47', 1, 0),
+(33, '2024-01-19 09:48:46', 1, 0),
+(34, '2024-01-19 09:52:36', 1, 0),
+(35, '2024-01-19 09:53:44', 1, 0),
+(36, '2024-01-19 09:54:08', 1, 0),
+(37, '2024-01-19 11:34:37', 1, 0),
+(38, '2024-01-19 11:35:57', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -189,6 +227,7 @@ CREATE TABLE `utenti` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `telefono` varchar(255) DEFAULT NULL,
+  `data_iscrizione` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_palestra` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -196,9 +235,10 @@ CREATE TABLE `utenti` (
 -- Dump dei dati per la tabella `utenti`
 --
 
-INSERT INTO `utenti` (`id`, `nome`, `cognome`, `data_nascita`, `email`, `password`, `telefono`, `id_palestra`) VALUES
-(1, 'Luigi', 'Riccardis', '2020-12-12', 'lriccardis022@gmail.com', 'password', '3714550374', 2),
-(2, 'Luigi', 'Lombardi', '2000-12-11', 'flombardi22@gmail.com', 'password', '3714550344', 2);
+INSERT INTO `utenti` (`id`, `nome`, `cognome`, `data_nascita`, `email`, `password`, `telefono`, `data_iscrizione`, `id_palestra`) VALUES
+(1, 'Luigi', 'Riccardis', '2020-12-12', 'lriccardis022@gmail.com', 'password', '3714550374', '2024-01-06 16:56:55', 2),
+(2, 'Luigi', 'Lombardi', '2000-12-11', 'flombardi22@gmail.com', 'password', '3714550344', '2024-01-06 16:56:55', 2),
+(3, 'Francesco', 'Lombardi', '1979-07-07', 'f.lombardi79@gmail.com', NULL, '3483760064', '2024-01-08 11:58:16', 2);
 
 --
 -- Indici per le tabelle scaricate
@@ -243,7 +283,7 @@ ALTER TABLE `utenti`
 -- AUTO_INCREMENT per la tabella `categorieesercizi`
 --
 ALTER TABLE `categorieesercizi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT per la tabella `esercizi`
@@ -261,13 +301,13 @@ ALTER TABLE `palestre`
 -- AUTO_INCREMENT per la tabella `schede`
 --
 ALTER TABLE `schede`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
