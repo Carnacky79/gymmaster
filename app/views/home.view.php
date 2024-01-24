0a8691de869ca145a35676ee285d308c44da18cf
Carnacky79 <f.lombardi79@gmail.com>
2024-01-24 23:10:19 +0100

    Added modal for selecting the right form for login

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -13,26 +13,12 @@
             <div class="m-b text-sm text-center">
                 Log In GymMaster
             </div>
-            <form name="form" action="<?= ROOT ?>/login" method="post">
-                <div class="md-form-group float-label">
-                    <input type="email" class="md-input" name="email" required>
-                    <label>Email</label>
-                </div>
-                <div class="md-form-group float-label">
-                    <input type="password" class="md-input" name="password" required>
-                    <label>Password</label>
-                </div>
-                <!--<div class="m-b-md">
-                    <label class="md-check">
-                        <input type="radio" name="admin" value="1"><i class="primary"></i> Entra come Admin
-                    </label>
+            <div class="m-b btn-groups">
+                <a class="btn primary btn-block" data-toggle="modal" data-target="#m-a-f" ui-toggle-class="fade-down" ui-target="#animate">Ciro</a>
+                <a class="btn danger btn-block" data-toggle="modal" data-target="#m-a-d" ui-toggle-class="fade-down" ui-target="#animate">Atleta</a>
 
-                    <label class="md-check">
-                        <input type="radio" name="admin" value="2"><i class="primary"></i> Entra come Utente
-                    </label>
-                </div>-->
-                <button type="submit" class="btn primary btn-block p-x-md">Entra</button>
-            </form>
+            </div>
+
         </div>
 
     </div>
@@ -40,6 +26,79 @@
     <!-- ############ LAYOUT END-->
 
 </div>
+<!-- .modal -->
+
+
+<div id="m-a-f" class="modal fade" data-backdrop="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Ciro</h5>
+            </div>
+            <div class="modal-body text-center p-lg">
+                <form name="form" action="<?= ROOT ?>/login" method="post">
+                    <div class="md-form-group float-label">
+                        <input type="email" class="md-input" name="email" required>
+                        <label>Email</label>
+                    </div>
+                    <div class="md-form-group float-label">
+                        <input type="password" class="md-input" name="password" required>
+                        <label>Password</label>
+                    </div>
+                    <!--<div class="m-b-md">
+                        <label class="md-check">
+                            <input type="radio" name="admin" value="1"><i class="primary"></i> Entra come Admin
+                        </label>
+
+                        <label class="md-check">
+                            <input type="radio" name="admin" value="2"><i class="primary"></i> Entra come Utente
+                        </label>
+                    </div>-->
+                    <button type="submit" class="btn primary btn-block p-x-md">Entra</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn danger p-x-md btn-block" data-dismiss="modal">Chiudi</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div>
+</div>
+
+<div id="m-a-d" class="modal fade" data-backdrop="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Atleta</h5>
+            </div>
+            <div class="modal-body text-center p-lg">
+                <form name="form" action="<?= ROOT ?>/login" method="post">
+                    <div class="md-form-group float-label">
+                        <input type="email" class="md-input" name="email" required>
+                        <label>Email</label>
+                    </div>
+                    <div class="md-form-group float-label">
+                        <input type="password" class="md-input" name="password" required>
+                        <label>Password</label>
+                    </div>
+                    <!--<div class="m-b-md">
+                        <label class="md-check">
+                            <input type="radio" name="admin" value="1"><i class="primary"></i> Entra come Admin
+                        </label>
+
+                        <label class="md-check">
+                            <input type="radio" name="admin" value="2"><i class="primary"></i> Entra come Utente
+                        </label>
+                    </div>-->
+                    <button type="submit" class="btn primary btn-block p-x-md">Entra</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn danger p-x-md btn-block" data-dismiss="modal">Chiudi</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div>
+</div>
+<!-- / .modal -->
 <?php require 'parts/footer.php'; ?>
 
 
